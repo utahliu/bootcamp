bac7fe0f89f732242c92a7cfe98c93211824b613
utah.liu <utah1127hk@gmail.com>
2024-10-03 21:01:19 +0800

    Update

---
+++ demo-datatype/src/DemoDatatype.java
@@ -0,0 +1,24 @@
+public class DemoDatatype {
+  public static void main(String[] args) {
+    //you can drop your notes here....
+
+    //data type
+    //from right to left -> assign value integer to a variable
+    int x = 1;
+    int y = 10;
+
+    int a = x + y; // 11
+
+    //Tool: System.out.println()
+    //print out
+    System.out.println(a); // 11
+
+    //Math Operation: +, -, *, /
+
+
+    //Math Operation: %
+    //remainder
+    int remainder = 10 % 3;
+    System.out.println(remainder); //
+  }
+}
